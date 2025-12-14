7f2bedc4280c3afa687b76e9728491853beaa77f
ssrhaso <hasaana2005@gmail.com>
2025-12-14 20:46:52 +0000

    FEAT: Implement setup_env.bat

--- setup_env.bat
+++ setup_env.bat
@@ -3,4 +3,6 @@ call .venv\Scripts\activate
 pip install --upgrade pip
 pip install torch torchvision transformers timm datasets wandb pytest black flake8
 pip freeze > requirements.txt
+
+.venv\Scripts\activate.ps1
 echo DONE!
